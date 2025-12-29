e7f7e69c44da93b44cbb73ac6ad13906e961bce8
biswas-bit <pokhrelbiswas53@gmail.com>
2025-12-29 20:33:25 +0545

    updated code cause artifact it self is pipeline

--- realstate/dashboard/ai_model.py
+++ realstate/dashboard/ai_model.py
@@ -7,14 +7,17 @@ from django.conf import settings
 
 class HousePriceModel:
     def __init__(self):
-        model_path = os.path.join(settings.BASE_DIR,'dashboard','xgb_pipeline_with_features.pkl')
+        model_path = os.path.join(
+            settings.BASE_DIR,
+            'dashboard',
+            'xgb_pipeline_with_features.pkl'
+            )
         
         if not os.path.exists(model_path):
             raise FileNotFoundError(f"Model file not found at {model_path}")
+        # Artifact it self is pipeline
         self.model = joblib.load(model_path)
-        artifact = joblib.load(model_path)
-        self.model = artifact['model']
-        self.features = artifact['features']
+        
 
     def predict(self, data: dict | list[dict]):
         df = pd.DataFrame(data if isinstance(data, list) else [data])
