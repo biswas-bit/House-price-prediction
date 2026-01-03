94469e2262c2ad463cd1d25a17cd16958faf83ad
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-03 13:06:18 +0545

    updated the api urls name

--- realstate/dashboard/urls.py
+++ realstate/dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path ('model-prediction/', views.model_prediction, name='model_prediction'),
     path('data-input-form/', views.Data_input_form, name='data_input_form'),
     path('api/market-insights/',views.get_market_insights, name='market_insights'),
-    path('api/get-recommendations/',views.get_recommendations,name='get_recommendations'),
+    path('api/get-recommendations/',views.get_recommendations,name='recommendations'),
+    path('api/analyze-property/',views.analyze_property,name="analyze_property")
 ]
