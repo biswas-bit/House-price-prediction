fe213f4f12c74f805b898ad087aeaa3ea62e8703
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-03 12:07:31 +0545

    added api url for recommendations

--- realstate/dashboard/urls.py
+++ realstate/dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path ('model-prediction/', views.model_prediction, name='model_prediction'),
     path('data-input-form/', views.Data_input_form, name='data_input_form'),
     path('api/market-insights/',views.get_market_insights, name='market_insights'),
+    path('api/get-recommendations/',views.get_recommendations,name='get_recommendations'),
 ]
